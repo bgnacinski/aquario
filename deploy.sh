eeffbdc9a7d55b86a8d68fa3699c848303af7c8e
unknown <borysgnacinski@gmail.com>
2025-06-10 16:06:38 +0200

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,10 @@
-cd /opt
-rm -r aquario
-
 useradd -M aquario
+chmod 750 -R ./
 
 sudo apt update
 sudo apt install python3 -y
 
 git clone https://github.com/bgnacinski/aquario
-
-chmod 750 -R aquario
-cd aquario
 
 pip install -r requirements.txt
 sudo cp aquario.service /etc/systemd/system/
