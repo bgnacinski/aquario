f52553ef13868ef164547447295e79cad868d8bd
unknown <borysgnacinski@gmail.com>
2025-06-10 16:37:05 +0200

    added ownership change in deploy.sh, updated gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 useradd -M aquario
-chmod 750 -R ./
+chown aquario:aquario -R ./
+chmod 760 -R ./
 
 sudo apt update
 sudo apt install python3 -y
@@ -8,3 +9,4 @@ git clone https://github.com/bgnacinski/aquario
 
 pip install -r watering-controller/requirements.txt
 sudo cp aquario.service /etc/systemd/system/
+sudo systemctl enable aquario
