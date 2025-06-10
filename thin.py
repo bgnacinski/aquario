ce104f25c4602a15a8f424f198dba63350878020
unknown <borysgnacinski@gmail.com>
2025-06-10 22:28:11 +0200

    unfortunately, realized that I only need this much code to get it running with better results :( Good thing is that I will probably reuse the code from main.py and lib/

---
+++ thin.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt
+import time
+import json
+
+def annouce_state(relay_pin:int, watering:bool):
+    payload = {
+        "watering": watering,
+        "relay_pin": relay_pin
+    }
+
+    print("Annoucing payload: ", payload)
+
+    mqtt_client.publish("watering/status", json.dumps(payload))
+
+def toggle_relay(relay_pin:int, enable:bool = True):
+    GPIO.setup(relay_pin, GPIO.OUT)
+    # due to reversed low_powered relays, logic is reversed
+    if enable:
+        GPIO.output(relay_pin, GPIO.LOW)
+
+    else:
+        GPIO.output(relay_pin, GPIO.HIGH)
+
+    annouce_state(relay_pin, enable)
+    print(f"Relay at {relay_pin} {'opened(off)' if enable else 'closed(on)'}.")
+
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("Connected to Aquario MQTT broker with result code: " + str(rc))
+    client.subscribe("watering/controller1")
+
+def on_message(client, userdata, msg):
+    try:
+        payload = json.loads(msg.payload)
+
+        toggle_relay(int(payload["pin"]), payload["enable"])
+
+    except (KeyError, ValueError) as e:
+        print("Error: " + repr(e) + " | Invalid payload: " + str(msg.payload))
+
+GPIO.setmode(GPIO.BCM)
+
+mqtt_client = mqtt.Client(client_id="aquario", callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+
+mqtt_client.username_pw_set("aquario", "aquario")
+
+mqtt_client.connect("10.0.3.21", 1883, 60)
+mqtt_client.loop_forever()
