c492f174df050a0121c9e68542202b642b4c5b56
unknown <borysgnacinski@gmail.com>
2025-06-10 16:14:38 +0200

    fixed requirements path

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ sudo apt install python3 -y
 
 git clone https://github.com/bgnacinski/aquario
 
-pip install -r requirements.txt
+pip install -r watering-controller/requirements.txt
 sudo cp aquario.service /etc/systemd/system/
